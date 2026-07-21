ae4fd3b9724490fc715c166a133b7a4e21db01de
shamnad-sherief <sshamnad03@gmail.com>
2026-07-21 21:48:43 +0530

    feat: define protocol command and response enums in crates/protocol

--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use murex_common::{Key, Value};
+
+#[derive(Debug)]
+pub enum Command {
+    Ping(Option<Value>),
+    Get(Key),
+    Set(Key, Value),
+    Delete(Key),
+    Help,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug)]
+pub enum Response {
+    Ok(Option<Value>),
+    Error(String),
+    NotFound,
+    Help(String),
 }
