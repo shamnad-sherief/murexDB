45286518ced84cdf28e0b884121189a65f6e0ced
shamnad-sherief <sshamnad03@gmail.com>
2026-07-21 21:41:55 +0530

    feat: introduce MurexError enum and workspace dependencies for common error handling

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MurexError {
+    #[error("I/O error: {0}")]
+    IOError(#[from] std::io::Error),
+
+    #[error("Invalid protocol frame: {0}")]
+    InvalidFrame(String),
+
+    #[error("Unknown OpCode: {0:#04x}")]
+    UnknownOpCode(u8),
+
+    #[error("Key not found")]
+    KeyNotFound,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub type Key = Vec<u8>;
+pub type Value = Vec<u8>;
+pub type Result<T> = std::result::Result<T, MurexError>;
